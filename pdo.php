ea2ac9b0b0cfaf5f944c1a29ef5c89a51a14812b
Leonardo Dezan <leo.dezan@gmail.com>
2023-01-14 13:25:51 -0300

    CRUD

--- pdo.php
+++ pdo.php
@@ -1,14 +1,21 @@
 <?php
 $servidor = "localhost";
 $banco = "brastem_db";
-$usuario = "roost";
+$usuario = "root";
 $senha = "";
 try{
     $conn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
-    echo "conectado";
+    $sql = "INSERT INTO tb_clientes (nome_cliente, cnpj_cpf) values ('Leonardo Dezan', '43126726879')";
+    $resultado = $conn->exec($sql);
+
+    $sql_listar = "SELECT * FROM tb_clientes WHERE id_clientes = '1'";
+    
+
+
 }catch(PDOException $erro){
     echo "erro ao conectar no banco de dados:   " . $erro->getMessage();
 }
+
 
 
 
