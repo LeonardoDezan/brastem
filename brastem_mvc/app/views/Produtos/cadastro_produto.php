592f896ba3438284960ad077925c32295adc1456
Leonardo Dezan <101656061+LeonardoDezan@users.noreply.github.com>
2023-02-01 12:33:46 -0300

    Update cadastro_produto.php

--- brastem_mvc/app/views/Produtos/cadastro_produto.php
+++ brastem_mvc/app/views/Produtos/cadastro_produto.php
@@ -13,11 +13,8 @@
 								</div>
 							</div>
 
-								<div class="col-1 mb-3">
-									 <span class="text-label">ID</span>
-									<input type="text" name="id_produto" value="<?php echo isset($produtos) ? $produtos->id_produto : null; ?>" class="form-campo" placeholder="Digite o nome do Produto" required>
-									<!-- <small href="" class="msg msg-vermelho h6 mt-1 p-1"><i class="fas fa-exclamation-triangle"></i> O Campo <b>Produto</b> é obrigatório <a href="javascript:;" onclick="fecharMsg()" class="fas fa-times float-right"></a></small> -->
-				
+								<div>
+									<input type="hidden" value="<?php echo isset($produtos) ? $produtos->id_produto : ""; ?>" name="id_produto" id="id_produto">				
 								</div>
 
 								<div class="col-8 mb-3">
@@ -30,7 +27,7 @@
 								<div class="col-3 mb-3">
 									 <span class="text-label">Código</span>
 									<input type="text" name="codigo" value="<?php echo isset($produtos) ? $produtos->codigo : ""; ?>" class="form-campo" placeholder="Digite o Código do Produto" required>
-									<small href="" class="msg msg-vermelho h6 mt-1 p-1"><i class="fas fa-exclamation-triangle"></i> O Campo <b>preço</b> é obrigatório <a href="javascript:;" onclick="fecharMsg()" class="fas fa-times float-right"></a></small>
+									<!-- <small href="" class="msg msg-vermelho h6 mt-1 p-1"><i class="fas fa-exclamation-triangle"></i> O Campo <b>preço</b> é obrigatório <a href="javascript:;" onclick="fecharMsg()" class="fas fa-times float-right"></a></small> -->
 				
 								</div>	
 								
@@ -59,7 +56,7 @@
 								
 								<div class="col-3 mb-3">
 									 <span class="text-label">estoque</span>
-									<input type="text" name="estoque" value="<?php echo isset($produtos) ? $produtos->estoque : ""; ?>" class="form-campo" placeholder="Digite a quantidade de Produtos no Estoque" required>
+									<input type="text" name="estoque" value="<?php echo isset($produtos) ? $produtos->estoque : ""; ?>" class="form-campo" required>
 									<!-- <small href="" class="msg msg-vermelho h6 mt-1 p-1"><i class="fas fa-exclamation-triangle"></i> O Campo <b>estoque estadual</b> é obrigatório <a href="javascript:;" onclick="fecharMsg()" class="fas fa-times float-right"></a></small> -->
 				
 								</div>
